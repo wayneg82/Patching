50f67384e370bcc3f693c01c51e9c95ec0fda9da
wayneg82 <60450488+wayneg82@users.noreply.github.com>
2021-12-01 17:56:04 +0000

    Update Pre-Patching.ps1

--- Pre-Patching.ps1
+++ Pre-Patching.ps1
@@ -5,6 +5,10 @@ Write-Host "Enabling and Starting Services"
 Set-Service -Name bits -StartupType Automatic
 Set-Service -Name wuauserv -StartupType Automatic
 Set-Service -Name ClickToRunSvc -DisplayName "Microsoft Office Click-to-Run Service" -StartupType Automatic
+Set-Service -Name gupdate -DisplayName "Google Update Service (gupdate)" -StartupType Manual
+Set-Service -Name gupdatem -DisplayName "Google Update Service (gupdatem)" -StartupType Manual
+Set-Service -Name edgeupdate -DisplayName "Microsoft Edge Update Service (edgeupdate)" -StartupType Manual
+Set-Service -Name edgeupdatem -DisplayName "Microsoft Edge Update Service (edgeupdatem)" -StartupType Manual
 Start-Service -Name bits
 Start-Service -Name wuauserv
 Start-Service -Name ClickToRunSvc
